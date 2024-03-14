ed81bea7de9e2b5179e931e08646e9dc5dca5ef4
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-03-14 16:10:21 +0530

    Used Functional Component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
+function Counter() {
+  const [count, setCount] = useState(0);
 
-  incrementCount = () => {
-    const curr = this.state.count;
-    this.setState({ count: curr + 1 });
+  const incrementCount = () => {
+    setCount(count + 1);
   };
 
-  render() {
-    return (
-      <>
-        <p>Button clicked {this.state.count} times</p>
-        <button onClick={this.incrementCount}>Click me</button>
-      </>
-    );
-  }
+  return (
+    <>
+      <p>Button clicked {count} times</p>
+      <button onClick={incrementCount}>Click me</button>
+    </>
+  );
 }
 
 export default Counter;
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
+
+//   incrementCount = () => {
+//     const curr = this.state.count;
+//     this.setState({ count: curr + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <p>Button clicked {this.state.count} times</p>
+//         <button onClick={this.incrementCount}>Click me</button>
+//       </>
+//     );
+//   }
+// }
+
+// export default Counter;
