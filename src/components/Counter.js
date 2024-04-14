6368f3ed0d68e639689a46072c0e468165999638
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-04-14 11:30:25 +0000

    submission

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -16,26 +16,3 @@ function Counter() {
 }
 
 export default Counter;
-
-// class Counter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { count: 0 };
-//   }
-
-//   incrementCount = () => {
-//     const curr = this.state.count;
-//     this.setState({ count: curr + 1 });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <p>Button clicked {this.state.count} times</p>
-//         <button onClick={this.incrementCount}>Click me</button>
-//       </>
-//     );
-//   }
-// }
-
-// export default Counter;
